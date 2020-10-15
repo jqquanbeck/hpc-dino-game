015c42e59618806fb10cf9d4d0ce2af72502a64a
Hanshaw <alhanshaw@login.cm.cluster>
2020-10-14 23:58:18 -0500

    CSV to Mat converter with simple TM algo

---
+++ CSVTemplateMatch/CMakeFiles/CSVTemplateMatch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSVTemplateMatch.dir/CSVTemplateMatch.cpp.o"
+  "CSVTemplateMatch"
+  "CSVTemplateMatch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CSVTemplateMatch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
