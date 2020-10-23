37ecebeaf60f68582052252375fe46f171e82a54
Hanshaw <alhanshaw@login.cm.cluster>
2020-10-22 23:41:25 -0500

    Working on multi-detect. Unable to replicate minMaxLoc results manually

---
+++ CV/MultiDetect/CMakeFiles/MultiDetect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MultiDetect.dir/MultiDetect.cpp.o"
+  "MultiDetect"
+  "MultiDetect.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MultiDetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
