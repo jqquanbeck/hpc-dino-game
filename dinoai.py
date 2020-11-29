0010a3266d4b8a2e95b534f008c0944036810656
Namikawa <gnamikawa@resgpu001.jacks.local>
2020-11-29 17:59:54 -0600

    ported main function in python

--- dinoai.py
+++ dinoai.py
@@ -1,5 +1,17 @@
 from translator import *
+import sys
 
-print("Hello world from python!")
+argc = len(sys.argv)
+argv = sys.argv
 
-#print( saveScreenshot("filepath") )
+print( f"Starting Dinoai with [{argc}] arguments: {str(argv)}" )
+
+if(argc != 3 ):
+    print("Provide the number of instances followed by the first display indentifier.\n")
+    exit(1)
+
+N = int( argv[1] )
+firstDisplay = int( argv[2] )
+
+print(f"\n\n==================== Starting DinoAI ====================\n");
+print(f"Batch size = {N}\n\n");
