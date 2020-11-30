fa640cd29076e0443339312b0cf8b2727d571071
Joel Quanbeck <joel.quanbeck@jacks.sdstate.edu>
2020-11-30 12:56:49 -0600

    Output errors to log

--- tools/run.sh
+++ tools/run.sh
@@ -23,7 +23,7 @@ echo "Starting with Xnest :"$firstX
 echo "Starting Xnest and google chrome"
 
 echo "starting at "$firstX
-echo "ending at "$(($firstX + $N))
+echo "ending at "$(($firstX + $N - 1))
 
 #read -p "Press enter to continue"
 
@@ -31,14 +31,14 @@ for (( i=$firstX; i<$(($firstX + $N)); i++))
 do
     export DISPLAY=$rootDisplay
     echo "Launching window "$i
-    Xnest :$i -geometry 600x150+$((20 + 610 * (($i - $firstX) / 5)))+$((50 + 200 * (($i - $firstX) % 5))) -bw 0 &
+    Xnest :$i -geometry 600x150+$((20 + 610 * (($i - $firstX) / 5)))+$((50 + 200 * (($i - $firstX) % 5))) -bw 0 &>> log.txt &
 done
 
 sleep 5
 
 for (( i=$firstX; i<$(($firstX + $N)); i++))
 do
-    google-chrome-stable --display=:$i --incognito --user-data-dir=/tmp/dino/$i --no-first-run &
+    google-chrome-stable --display=:$i --incognito --user-data-dir=/tmp/dino/$i --no-first-run &>> log.txt &
 done
 
 sleep 15
